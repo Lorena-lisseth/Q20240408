0696c42ce28acccca655483ea2c32a13a2237ee3
Lorena Lisseth Vasquez Godoy <liliyong23cat@gmail.com>
2024-04-08 09:30:03 -0600

    Se creo la clase personaq en la capa de entidad de negocio

---
+++ Q20240408.EntidadesDeNegocio/PersonaQ.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Q20240408.EntidadesDeNegocio
+{
+    public class PersonaQ
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(60)]
+        public string NombreQ { get; set; }
+        [Required]
+        [StringLength(60)]
+        public string ApellidoQ { get; set; }
+        public DateTime FechaNacimientoQ { get; set; }
+        public decimal SueldoQ { get; set; }
+        public byte StatusQ { get; set; }
+    }
+}
